e721460fede67db915f43c4078f5ed3e892530a1
matheusiscte <mpclq@iscte-iul.pt>
2023-03-12 00:44:14 +0000

    ends_with 5

--- rock_paper_scissors_Finale/script.js
+++ rock_paper_scissors_Finale/script.js
@@ -25,6 +25,7 @@ showWall.addEventListener('click', () => {
 //função para mudar de section
 const allSections = document.querySelectorAll('section');
 
+
 window.addEventListener('DOMContentLoaded', () => {
   const activateSection = (id) => {
   for(const section of allSections) {
@@ -32,9 +33,12 @@ window.addEventListener('DOMContentLoaded', () => {
       intro.remove();
   }
   //adicionar a classe is-active ao id que queremos
-  document.querySelector('[data-id="' + id + '"]').classList.add('is-active');
-  };
-  allSections[0].addEventListener('click', () => {
+  setTimeout(() => {
+    document.querySelector('[data-id="' + id + '"]').classList.add('is-active');
+  }, 2000);
+};
+  
+  showWall.addEventListener('click', () => {
     activateSection(2);
 });
 });
@@ -65,10 +69,18 @@ const getComputerChoice = () =>{
   return choices[randomIndex];
 }
 
+let playerSelection = ""
+let computerSelection=""
+
+
 const playRound = (playerSelection, computerSelection) =>{
+
+  if(scoreUser >= 5 || scoreCPU >= 5){
+    return;
+  }
         
   if(playerSelection === computerSelection){
-      lastResult.textContent = "This round was a drawlez goe"
+      lastResult.textContent = "This round was a draw"
   }
  if(playerSelection === "rock" && computerSelection === "paper"){
   scoreCPU++;
@@ -99,47 +111,29 @@ const playRound = (playerSelection, computerSelection) =>{
   updateScore();
   lastResult.textContent =  "You Win! Scissors beats paper"
  }
-}
+ if (scoreCPU >= 5 || scoreUser >= 5) {
+  if (scoreCPU > scoreUser) {
+    finalScore.textContent = "Final Score -> CPU wins!!!";  
+  } else if (scoreCPU < scoreUser) {
+    finalScore.textContent = "Final Score -> User wins";   
+  }
 
-        document.getElementById('rock').addEventListener("click", () =>{
-          playRound('rock', getComputerChoice());
-        });
-        
-        document.getElementById('paper').addEventListener("click", () =>{
-          playRound('paper', getComputerChoice());
-        });
-        
-        document.getElementById('scissors').addEventListener("click", () =>{
-          playRound('scissors', getComputerChoice());
-        });
+}}
 
-const game = () => {
-  let i = 0
-  while(scoreUser < 5 && scoreCPU < 5) {
-        console.log(playRound(playerSelection, computerSelection));
-          const computerSelection = getComputerChoice();
-          i++;
-          playRound();
-      }
+document.getElementById('rock').addEventListener("click", () =>{
+  playerSelection = "rock"
+  playRound(playerSelection, getComputerChoice());
+});
 
-      if (scoreCPU === scoreUser){
-        finalScore.textContent = "Final Score -> its a draw";
-      }
-      else if(scoreCPU>scoreUser){
-        finalScore.textContent = "Final Score -> CPU wins!!!";  
-      } 
-      else{
-        finalScore.textContent = "Final Score -> User wins";   
-      }
-}
-//event listeners para os buttons
-/* const buttons = document.querySelectorAll('.button');
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    playRound(button.id, getComputerChoice());
-  });
-}); */
+document.getElementById('paper').addEventListener("click", () =>{
+  playerSelection = "paper"
+  playRound(playerSelection, getComputerChoice());
+});
+
+document.getElementById('scissors').addEventListener("click", () =>{
+  playerSelection = "scissors"
+  playRound(playerSelection, getComputerChoice());
+});
 
 
-game();
-  
+
